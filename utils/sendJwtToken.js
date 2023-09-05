c96ad556cc938bbdf501b7dbad7dcda216e94239
harsh gupta <118052863+Harsh605@users.noreply.github.com>
2023-09-06 03:13:02 +0530

    Update sendJwtToken.js

--- utils/sendJwtToken.js
+++ utils/sendJwtToken.js
@@ -6,8 +6,8 @@ const sendToken =(user,statusCode,res)=>{
     const options ={
         httpOnly:true,
         expires: new Date(Date.now()+ process.env.COOKIE_TIME *60*60*60*1000),
-        // sameSite:"None",
-        // secure:true
+        sameSite:"None",
+        secure:true
 
     }
     res.status(statusCode).cookie('token',token,options).json({
